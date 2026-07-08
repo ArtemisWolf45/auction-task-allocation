17ebdd486720ce6f7f5710681b9cd66050d99948
Nikita Razdan <nikita9razdan@gmail.com>
2026-07-08 13:55:29 +0530

    Add bidding functions to calculate distance and bid for tasks

---
+++ auction/bidding.py
@@ -0,0 +1,15 @@
+def calculate_distance(worker, task):
+    # Calculates absolute distance of the positions on a number line
+    return abs(worker.position - task.position)
+
+def calculate_bid(worker, task, load_penalty=3):
+    if not(worker.can_take_task()):
+        return None
+    
+    distance = calculate_distance(worker, task)
+
+    if distance > task.deadline: # Checks if worker will reach task in time
+        return None
+    
+    bid = distance + (worker.current_workload() * load_penalty) # penalises workers with larger workloads
+    return bid
